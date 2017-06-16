f07f61869dc20af182d1200a4090cd1276dd9d7b
Saurabh Bipin Chandra <srbhchandra@gmail.com>
2017-06-15 18:23:42 -0700

    all changes
    Signed-off-by: Saurabh Bipin Chandra <srbhchandra@gmail.com>

--- util/cifar10_loader.py
+++ util/cifar10_loader.py
@@ -1,5 +1,13 @@
+""" Loader module for CIFAR 10 dataset."""
 import pickle
 import numpy as np
+
+
+def unpickle(file):
+    with open(file, 'rb') as opened_file:
+        file_dict = pickle.load(opened_file, encoding='bytes')
+    return file_dict
+
 
 class Cifar10Loader():
     """
@@ -10,21 +18,16 @@ class Cifar10Loader():
     test error rate of 15% (without data augmentation) using the architecture of the net that got 18%.
     """
     def __init__(self):
-        self.train_data_files = ['data_batch_1', 'data_batch_2' ,'data_batch_3', 'data_batch_4', 'data_batch_5']
-        self.test_data_file  = 'test_batch'
+        self.train_data_files = ['data_batch_1', 'data_batch_2', 'data_batch_3', 'data_batch_4', 'data_batch_5']
+        self.test_data_file = 'test_batch'
         self.img_size = 32
         self.num_channels = 3
         self.img_size_flat = self.img_size * self.img_size * self.num_channels
         self.num_classes = 10
         self.images_per_file = 10000
 
-    def _unpickle(self, file):
-        with open(file, 'rb') as fo:
-            dict = pickle.load(fo, encoding='bytes')
-        return dict
-
     def _get_data_label_from_file(self, file):
-        data_dict = self._unpickle('/'.join([self.dataset_path, file]))
+        data_dict = unpickle('/'.join([self.dataset_path, file]))
         return data_dict[b'data'], np.array(data_dict[b'labels'])
 
     def load_cifar10(self, data_path='../Datasets/cifar-10'):
@@ -49,6 +52,6 @@ class Cifar10Loader():
         X_test = X_test.reshape(X_test.shape[0], self.num_channels, self.img_size, self.img_size).transpose(0, 2, 3, 1).astype("float")
         X_test = np.array(X_test, dtype=float) / 255.0
 
-        labels_dict = self._unpickle('/'.join([self.dataset_path, "batches.meta"]))
+        labels_dict = unpickle('/'.join([self.dataset_path, "batches.meta"]))
         labels = [bytes.decode(a) for a in labels_dict[b'label_names']]
         return X_train, y_train, X_test, y_test, labels
